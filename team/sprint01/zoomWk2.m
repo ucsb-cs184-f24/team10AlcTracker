4449cbd428e366ca8b0fe46b510c1806c26aa347
mayarosenbaum <30633019+mayarosenbaum@users.noreply.github.com>
2024-10-10 17:15:14 -0700

    Create zoomWk2.m

---
+++ team/sprint01/zoomWk2.m
@@ -0,0 +1,3 @@
+# Meeting notes for Zoom meeting 10/10 (Week 2)
+
+In this meeting, my group got together virtually to discuss or progress on the HelloWorld Assignment. We went over (and resolved) some problems with the emulator. After some discussion, we successfully got our HelloWorld projects running. We also went over our app scenario again.
