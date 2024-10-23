5298e3085899f2b9172cec4abc96b2f275a4e28c
madison <longmadison@gmail.com>
2024-10-23 01:32:54 -0700

    Added base code, testing if it works

---
+++ BACtrack/app/src/main/java/com/example/bactrack/MainActivity.kt
@@ -0,0 +1,220 @@
+package com.example.bactrack
+
+import android.annotation.SuppressLint
+import android.graphics.drawable.Icon
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.SportsBar
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.SportsBar
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.bactrack.ui.theme.BACtrackTheme
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+
+
+
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BACtrackTheme {
+                val items = listOf(
+                    BottomNavigationItem(
+                        title = "Home",
+                        selectedIcon = Icons.Filled.Home,
+                        unselectedIcon = Icons.Outlined.Home,
+                        hasNews = false
+                    ),
+                    BottomNavigationItem(
+                        title = "Your History",
+                        selectedIcon = Icons.Filled.SportsBar,
+                        unselectedIcon = Icons.Outlined.SportsBar,
+                        hasNews = false
+                    ),
+                    BottomNavigationItem(
+                        title = "Settings",
+                        selectedIcon = Icons.Filled.Settings,
+                        unselectedIcon = Icons.Outlined.Settings,
+                        hasNews = false
+                    )
+                )
+                var selectedItemIndex by rememberSaveable { mutableStateOf(0) }
+
+                var navController = rememberNavController()
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color(0xFFADD8E6)
+                ) {
+                    Scaffold(
+                        bottomBar = {
+                            BottomNavigationBar(
+                                items = items,
+                                selectedItemIndex = selectedItemIndex,
+                                onItemSelected = { index ->
+                                    selectedItemIndex = index
+                                    when(index) {
+                                        0 -> navController.navigate("home")
+                                        1 -> navController.navigate("history")
+                                        2 -> navController.navigate("settings")
+                                    }
+                                }
+                            )
+                        }
+                    ) { paddingValues -> // This will ensure our main content is not covered by the bottom bar
+//                        Surface(
+//                            modifier = Modifier
+//                                .fillMaxSize()
+//                                .padding(paddingValues) ){}
+                        NavHost(
+                            navController = navController,
+                            startDestination = "home",
+                            modifier = Modifier.padding(paddingValues)
+                        ) {
+                            composable("home") { HomeScreen() }
+                            composable("history") { HistoryScreen() }
+                            composable("settings") { SettingsScreen() }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+data class BottomNavigationItem (
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    //this will represent a number of notifications the icon will contain (optional)
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+){ }
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BACtrackTheme {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavigationItem>,
+    selectedItemIndex: Int,
+    onItemSelected: (Int) -> Unit
+) {
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedItemIndex == index,
+                onClick = { onItemSelected(index)},
+                label = { Text(text = item.title) },
+                icon = {
+                    BadgedBox(
+                        badge = {
+                            if(item.badgeCount != null) {
+                                Badge{ Text(text = item.badgeCount.toString()) }
+                            } else if(item.hasNews) { Badge()}
+                        }
+                    ) {
+                        Icon(
+                            imageVector = if(index == selectedItemIndex) {
+                                item.selectedIcon
+                            } else {
+                                item.unselectedIcon
+                            },
+                            contentDescription = item.title
+                        )
+                    }
+                }
+            )
+
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFFADD8E6)
+
+    ) {
+        Text(
+            text = "Home Screen",
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+fun HistoryScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFFADD8E6)
+    ) {
+        Text(
+            text = "History Screen",
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+fun SettingsScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFFADD8E6)
+    ) {
+        Text(
+            text = "Settings Screen",
+            color = Color.Black
+        )
+    }
+}
+
