2e96af1aad0b86c5496356717273ce4e2773ad03
Tex <elijah_c_anderson@ucsb.edu>
2024-11-03 14:03:41 -0800

    rosillo-m_hw3_experiment

--- BACtrack/app/build.gradle.kts
+++ BACtrack/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.bactrack"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
