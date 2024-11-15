c13201d43f77cb0c452686fee0eb2a393e4295da
Mariana Rosillo <rosillo@ucsb.edu>
2024-11-15 13:27:31 -0800

    Added the correct BAC calculation that takes on the time since drinking and the user's weight and birth sex

--- BACtrack/app/src/main/java/com/example/bactrack/SessionManager.kt
+++ BACtrack/app/src/main/java/com/example/bactrack/SessionManager.kt
@@ -6,14 +6,40 @@ import androidx.compose.runtime.setValue
 
 object SessionManager {
     var currentSession by mutableStateOf(CurrentSession())
+    var firstDrinkTime: Long? = null
 
-        //this value will dynamically update thanks to the get function
     val totalDrinks: Int
         get() = currentSession.numBeers + currentSession.numWine + currentSession.numShots + currentSession.numCocktails
 
-    //this will return total mass of alcohol consumed in grams
     val totalAlcMass: Double
-        get() = (currentSession.numBeers * 14.0) + (currentSession.numWine * 20.8) + (currentSession.numShots * 14) + (currentSession.numCocktails * 27.1)
-}
-//this is a global object that will be used to store the current session
+        get() = (currentSession.numBeers * 14.0) +
+                (currentSession.numWine * 20.8) +
+                (currentSession.numShots * 14.0) +
+                (currentSession.numCocktails * 27.1)
 
+    var bac by mutableStateOf(0.0)
+
+    fun addDrink(drinkType: String) {
+        currentSession = when (drinkType) {
+            "beer" -> currentSession.copy(numBeers = currentSession.numBeers + 1)
+            "wine" -> currentSession.copy(numWine = currentSession.numWine + 1)
+            "shot" -> currentSession.copy(numShots = currentSession.numShots + 1)
+            "cocktail" -> currentSession.copy(numCocktails = currentSession.numCocktails + 1)
+            else -> currentSession
+        }
+        if (firstDrinkTime == null) firstDrinkTime = System.currentTimeMillis()
+        recalculateBAC()
+    }
+
+    fun recalculateBAC() {
+        val elapsedTime = getElapsedTimeInHours()
+        val user = PersonManager.mainUser
+        bac = calculateBAC(totalAlcMass, user.weight, if (user.sex) "male" else "female", elapsedTime)
+    }
+
+    private fun getElapsedTimeInHours(): Double {
+        return if (firstDrinkTime != null) {
+            (System.currentTimeMillis() - firstDrinkTime!!) / 3600000.0
+        } else 0.0
+    }
+}
